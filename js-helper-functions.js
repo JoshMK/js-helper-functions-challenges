12affe6ea267bc557d4d68d5cf1c813e1257aa78
Joshua <joshuakirwin@gmail.com>
2020-04-23 22:54:32 -0500

    added additional challenge solved.

--- js-helper-functions.js
+++ js-helper-functions.js
@@ -1,4 +1,4 @@
-//get groups of numbers from an array
+//getGroupedNumbersFromArray - returns the number of grouped numbers (pairs by default) from an array
 function getGroupedNumbersFromArray(pairArray, numGroupedBy = 2) {
 let numMatches,
 numGroupedMatches = 0;
@@ -17,4 +17,22 @@ for (let i = 0; i < pairArray.length; i++) {
     numMatches = 0;
 });
 return numGroupedMatches;
+}
+
+//getNumberMatchingStringsByQuery - returns an array containing a number for each time the checked array's string matched an array of queries
+function getNumberMatchingStringsByQuery(stringsArray, queriesArray) {
+    let numMatches = 0,
+    matchesArray = [];
+    //for each string query, compare it to the array of strings
+    queriesArray.forEach(query => {
+        for (let i = 0; i < stringsArray.length; i++) {
+            if (query === stringsArray[i]) {
+                numMatches += 1;
+            }
+        }
+        //add matches to array and reset counter
+        matchesArray.push(matches);
+        matches = 0;
+    });
+    return matchesArray;
 }
