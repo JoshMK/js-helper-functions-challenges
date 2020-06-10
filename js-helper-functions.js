c76615f77cbbd6eed20c63802fcc29c400a113e7
Joshua M Kirwin <MKirwin@as.tamu.edu>
2020-06-10 13:59:40 -0500

    function fixes + test html file.

--- js-helper-functions.js
+++ js-helper-functions.js
@@ -1,47 +1,32 @@
 //getGroupedNumbersFromArray - returns the number of grouped numbers (pairs by default) from an array
 function getGroupedNumbersFromArray(pairArray, numGroupedBy = 2) {
-let numMatches,
-numGroupedMatches = 0;
-//get unique entries from array
-const uniqueNumbers = Array.from(new Set(pairArray));
-uniqueNumbers.forEach(number => {
-for (let i = 0; i < pairArray.length; i++) {
-    if (pairArray[i] === number) {
-        //tally each unique element
-        numMatches += 1;
-        }
-    }
-    //calculate the grouped numbers
-    numGroupedMatches += Math.floor(numMatches / numGroupedBy);
-    //reset the match tally for the next unique number
-    numMatches = 0;
-});
-return numGroupedMatches;
-}
-
-//getNumberMatchingStringsByQuery - returns an array containing a number for each time the checked array's string matched an array of queries
-function getNumberMatchingStringsByQuery(stringsArray, queriesArray) {
-    let numMatches = 0,
-    matchesArray = [];
-    //for each string query, compare it to the array of strings
-    queriesArray.forEach(query => {
-        for (let i = 0; i < stringsArray.length; i++) {
-            if (query === stringsArray[i]) {
-                numMatches += 1;
+    let numMatches = 0;
+    let numGroupedMatches = 0;
+    //get unique entries from array
+    const uniqueNumbers = Array.from(new Set(pairArray));
+    console.log(uniqueNumbers);
+    uniqueNumbers.forEach(number => {
+    for (let i = 0; i < pairArray.length; i++) {
+        if (pairArray[i] === number) {
+            //tally each unique element
+            numMatches += 1;
             }
         }
-        //add matches to array and reset counter
-        matchesArray.push(matches);
-        matches = 0;
+        //calculate the grouped numbers
+        numGroupedMatches += Math.floor(numMatches / numGroupedBy);
+        //reset the match tally for the next unique number
+        numMatches = 0;
     });
-    return matchesArray;
-}
+    return numGroupedMatches;
+    }
+    
+    //createObjectWithRemovedProperty - returns an object from an existing object with a specified property removed
+    function createObjectWithRemovedProperty(oldObj, prop) {
+    let newObjMinusProp = {...oldObj};
+    if (newObjMinusProp.hasOwnProperty(prop)) {
+        delete newObjMinusProp[prop];        
+    }
+    return newObjMinusProp;
+    }
 
-//createObjectWithRemovedProperty - returns an object from an existing object with a specified property removed
-function createObjectWithRemovedProperty(oldObj, prop) {
-let newObjMinusProp = {...oldObj};
-if (newObjMinusProp.hasOwnProperty(prop)) {
-    delete newObjMinusProp[prop];        
-}
-return newObjMinusProp;
-}
+    //
